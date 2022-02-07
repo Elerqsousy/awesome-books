7c72108782191ab2e41d534ff06ddc2e4306b50b
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-02-07 20:58:48 +0200

    Add linters + update readme file

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# awesome-books
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# the MUSICAL
+
+> Sample Design of a book List.
+
+## Screensot
+
+![screenshot](./screenshot.png) 
+
+
+## Built With
+
+- HTML
+- JS
+## Getting Started
+
+This is a simple static website made with HTML5 and JavaScript. There is not specific requirements for building the project.
+## Live Demo
+
+Check the [Live Demo](https://elerqsousy.github.io/awesome-books/) deployed on github pages.
+## Authors
+
+👤 **Mahmoud Rizk**
+
+- GitHub: [@Elerqsousy](https://github.com/Elerqsousy)
+
+👤 **DAgic Zewdu**
+
+- GitHub: [@Dagic-zewdu ](https://github.com/Dagic-zewdu)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is [MIT](./LICENSE) licensed.
--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 let bookList = [];
-const formBook=document.querySelector('#book-form')
+const formBook = document.querySelector('#book-form');
 const submitBut = document.querySelector('.user-input-submit');
-const form = document.querySelectorAll('.form-input'); //form selection
+const form = document.querySelectorAll('.form-input');
 const pageBookList = document.querySelector('.book-list');
 
-const removeBook=(id)=>{
-const newBookList=bookList.filter((book)=>book.id !== parseInt(id))
-bookList=newBookList
-pageBookList.innerHTML=``
-insertContent()
-updateStorage()
-}
+const removeBook = (id) => {
+  const newBookList = bookList.filter((book) => book.id !== parseInt(id));
+  bookList = newBookList;
+  pageBookList.innerHTML = ``;
+  insertContent();
+  updateStorage();
+};
 function insertContent() {
   bookList.forEach((book) => {
     pageBookList.innerHTML += `
@@ -22,10 +22,10 @@ function insertContent() {
      </article>
          `;
   });
-  const removeBtn=document.querySelectorAll('.remove-btn')
-  removeBtn.forEach(button=>{
-    button.addEventListener('click',()=>removeBook(button.id))
-  })
+  const removeBtn = document.querySelectorAll('.remove-btn');
+  removeBtn.forEach((button) => {
+    button.addEventListener('click', () => removeBook(button.id));
+  });
 }
 
 if (!localStorage.getItem('booklist')) {
@@ -45,14 +45,14 @@ function updateUserInterface() {
   insertContent();
 }
 //random id
-const randomId=()=>Math.round(Math.random()*10000) //random id function
+const randomId = () => Math.round(Math.random() * 10000); //random id function
 
 function onSubmition(e) {
   e.preventDefault();
   const title = form[0].value;
   const author = form[1].value;
   const input = {
-    id:randomId(),
+    id: randomId(),
     title: title,
     author: author,
   };
@@ -62,10 +62,4 @@ function onSubmition(e) {
   updateStorage();
 }
 
-formBook.addEventListener('submit',onSubmition)
-//submitBut.addEventListener('click', onSubmition);
-
-// "Remove" button:
-
-//     The correct book is removed from the collection.
-//     The correct book dissapears from the page.
+formBook.addEventListener('submit', onSubmition);
