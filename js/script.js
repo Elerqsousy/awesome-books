58f3ee9c1fd366cdad39ea1c7d77d6f13beaf0ff
Dagi <dagi.zewdu.dz@gmail.com>
2022-02-07 21:43:29 +0300

    Added remove functionality for the remove button

--- js/script.js
+++ js/script.js
@@ -1,19 +1,31 @@
 let bookList = [];
+const formBook=document.querySelector('#book-form')
 const submitBut = document.querySelector('.user-input-submit');
-const form = document.querySelectorAll('.form-input');
+const form = document.querySelectorAll('.form-input'); //form selection
 const pageBookList = document.querySelector('.book-list');
 
+const removeBook=(id)=>{
+const newBookList=bookList.filter((book)=>book.id !== parseInt(id))
+bookList=newBookList
+pageBookList.innerHTML=``
+insertContent()
+updateStorage()
+}
 function insertContent() {
-  bookList.forEach((e) => {
+  bookList.forEach((book) => {
     pageBookList.innerHTML += `
          <article class="book">
-         <h4 class="book-name">${e.title}</h4>
-         <p class="book-writer">${e.author}</p>
-         <button class="remove ${bookList.indexOf(e)}" type="submit">Remove</button>
+         <h4 class="book-name">${book.title}</h4>
+         <p class="book-writer">${book.author}</p>
+         <button class="remove-btn" id=${book.id} type="button" >Remove</button>
          <hr>
      </article>
          `;
   });
+  const removeBtn=document.querySelectorAll('.remove-btn')
+  removeBtn.forEach(button=>{
+    button.addEventListener('click',()=>removeBook(button.id))
+  })
 }
 
 if (!localStorage.getItem('booklist')) {
@@ -32,11 +44,15 @@ function updateUserInterface() {
   bookList = JSON.parse(retrievedData);
   insertContent();
 }
+//random id
+const randomId=()=>Math.round(Math.random()*10000) //random id function
 
-function onSubmition() {
+function onSubmition(e) {
+  e.preventDefault();
   const title = form[0].value;
   const author = form[1].value;
   const input = {
+    id:randomId(),
     title: title,
     author: author,
   };
@@ -46,7 +62,8 @@ function onSubmition() {
   updateStorage();
 }
 
-submitBut.addEventListener('click', onSubmition);
+formBook.addEventListener('submit',onSubmition)
+//submitBut.addEventListener('click', onSubmition);
 
 // "Remove" button:
 
