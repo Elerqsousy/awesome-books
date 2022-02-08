da82c55176e1b5aa677beaadd97b86190ca5ae83
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-02-08 08:52:54 +0200

    fix linters error

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@ function onSubmition(e) {
     title,
     author,
   };
-  bookList= bookList.concat(input);
+  bookList = bookList.concat(input);
   pageBookList.innerHTML = '';
   insertContent();
   updateStorage();
